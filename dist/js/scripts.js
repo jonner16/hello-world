a6abc6a0b97759a240fadd7dca0f7fca8f805755
Jonah <jonahlavi@yahoo.com>
2023-03-08 13:50:28 -0800

    moved portfolio repo to this one

---
+++ dist/js/scripts.js
@@ -0,0 +1,36 @@
+// // TOP WAVE ANIMATION //
+const container = document.getElementById("wave-container");
+
+const animateWave = x => {
+    const wave = document.createElement("div");
+
+    wave.className = "wave jl-purple";
+
+
+    wave.style.left = x.toString() + 'px';
+
+    container.appendChild(wave);
+
+    setTimeout(() => container.removeChild(wave), 2000);
+}
+
+window.onmousemove = m => animateWave(m.clientX);
+
+
+// PDF READER //
+const pdfPath = "/dist/assets/resume.pdf";
+const pdfViewer = document.querySelector('#pdfViewer');
+
+fetch(pdfPath)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to load PDF');
+    }
+    return response.blob();
+  })
+  .then(pdfBlob => {
+    pdfViewer.data = URL.createObjectURL(pdfBlob);
+  })
+  .catch(error => {
+    console.error(error);
+  });
